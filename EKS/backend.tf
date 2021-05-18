cbc97a30d247389053eca8a3106295e50b87a8b8
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2021-05-18 11:38:45 +0100

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "jonaa"
+    organization = "jasonb"
 
     workspaces {
-      name = "Test-Workspace"
+      name = "EKS-Workspace"
     }
   }
 }
