32679340af01251839c46e2209a5784ed777491b
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2021-05-18 11:41:12 +0100

    Update vpc.tf

--- EKS/vpc.tf
+++ EKS/vpc.tf
@@ -7,14 +7,14 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "jonaa-eks"
+  cluster_name = "jasonb-eks"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "jonaa-vpc"
+  name                 = "jasonb-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
