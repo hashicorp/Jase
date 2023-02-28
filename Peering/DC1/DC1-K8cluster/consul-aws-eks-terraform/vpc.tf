dfbd220349c3b9020f28b95f6ffd082e53d1c48f
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-02-28 14:16:28 +0000

    Update vpc.tf

--- Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
+++ Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "jasonb3-eks-${random_string.suffix.result}"
+  cluster_name = "cluster1-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
