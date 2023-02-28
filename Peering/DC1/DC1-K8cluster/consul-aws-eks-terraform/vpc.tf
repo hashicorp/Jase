340cc077f2b831322c67dd678222cf22a94cf596
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-02-28 14:18:32 +0000

    Update vpc.tf

--- Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
+++ Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.0"
 
-  name                 = "jasonb-vpc"
+  name                 = "cluster1-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
