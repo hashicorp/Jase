5767ed1f5d83a51725ba9bec6919a956bddbc6e4
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-02-28 21:01:53 +0000

    Update eks-cluster.tf

--- Peering/DC3/DC3-K8cluster/consul-aws-eks-terraform/eks-cluster.tf
+++ Peering/DC3/DC3-K8cluster/consul-aws-eks-terraform/eks-cluster.tf
@@ -13,14 +13,14 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "jaseb-group-1"
+      name                          = "cluster3-group-1"
       instance_type                 = "t2.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
-      name                          = "jaseb-group-2"
+      name                          = "cluster3-group-2"
       instance_type                 = "t2.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
