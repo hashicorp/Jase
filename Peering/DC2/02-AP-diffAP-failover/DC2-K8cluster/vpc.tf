2e7b147dea903d274d2869352922ce52e9daf510
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-02-28 14:17:19 +0000

    Update vpc.tf

--- Peering/DC2/02-AP-diffAP-failover/DC2-K8cluster/vpc.tf
+++ Peering/DC2/02-AP-diffAP-failover/DC2-K8cluster/vpc.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "jasonb2-eks-${random_string.suffix.result}"
+  cluster_name = "cluster2-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.0"
 
-  name                 = "jasonb-vpc"
+  name                 = "cluster2-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
