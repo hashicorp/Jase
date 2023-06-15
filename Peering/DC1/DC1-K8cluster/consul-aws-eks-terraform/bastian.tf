9034fef661233daa70588f4911a923fa95ce0edc
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-06-15 10:55:33 +0100

    dc1 TF update

--- Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/bastian.tf
+++ Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/bastian.tf
@@ -1,12 +1,25 @@
-# --- compute/main.tf  --- #
+# --- bastian.tf  --- #
 
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/*ubuntu-jammy-22.04-amd64-server-*"]
+  }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 
 resource "aws_instance" "bastian" {
   instance_type = "t2.micro"
-  ami           = "ami-081c75eaeac28ac34"
+  ami           = data.aws_ami.ubuntu.id
   vpc_security_group_ids = [
     aws_security_group.bastian_sg.id
   ]
--- Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
+++ Peering/DC1/DC1-K8cluster/consul-aws-eks-terraform/vpc.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "eu-west-1"
   description = "AWS region"
 }
 
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "cluster1-eks-${random_string.suffix.result}"
+  cluster_name = "jaseb-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -22,11 +22,12 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.0"
 
-  name                 = "cluster1-vpc"
+  name                 = "jaseb-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  database_subnets     = ["10.0.21.0/24", "10.0.22.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
