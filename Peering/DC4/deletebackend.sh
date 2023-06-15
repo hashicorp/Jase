cb8758b82c4af69e42909948ddb083a320b2e054
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-06-15 10:38:32 +0100

    DC4 update

---
+++ Peering/DC4/deletebackend.sh
@@ -0,0 +1,6 @@
+kubectl delete deployment backend
+kubectl delete service backend
+kubectl delete serviceaccount backend
+kubectl delete servicedefaults backend
+kubectl delete serviceresolver backend
+kubectl delete serviceintentions backend
