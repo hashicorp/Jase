a7dfd023dc0d71e62d1805111b4a1ffbb387baec
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2023-09-26 14:54:33 +0100

    Update eks-cluster.tf

--- EKS/eks-cluster.tf
+++ EKS/eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
-  cluster_version = "1.18"
+  cluster_version = "1.22"
   subnets         = module.vpc.public_subnets
 
   vpc_id = module.vpc.vpc_id
