e337b2ac93ab78593debedcc959960b58eab5faa
jasebroadbent <81739850+jasebroadbent@users.noreply.github.com>
2024-01-25 12:30:17 +0000

    tf update

---
+++ SA-Tech/EKS/DC1/bastian.tf
@@ -0,0 +1,33 @@
+# --- bastian.tf  --- #
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/*ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "bastian" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [
+    aws_security_group.bastian_sg.id
+  ]
+  subnet_id = module.vpc.public_subnets[0]
+  key_name  = aws_key_pair.demo.key_name
+  tags = {
+    Name = "bastion"
+  }
+
+
+}
